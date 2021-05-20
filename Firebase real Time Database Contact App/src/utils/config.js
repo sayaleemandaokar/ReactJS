3cf51e7378d7db1022508537ca50d7bfffe4c478
sayaleemandaokar <58383890+sayaleemandaokar@users.noreply.github.com>
2021-05-20 21:15:05 +0530

    Create config.js

---
+++ Firebase real Time Database Contact App/src/utils/config.js
@@ -0,0 +1,42 @@
+//TODO: DONE: add firebase configuration and export it
+export const firebaseConfig = {
+    apiKey: "AIzaSyD9ThnAmna3M1dX9hLv_rYIBPUqqW-MunU",
+    authDomain: "gitappmay2021.firebaseapp.com",
+    projectId: "gitappmay2021",
+    storageBucket: "gitappmay2021.appspot.com",
+    messagingSenderId: "170365187456",
+    appId: "1:170365187456:web:6baa566de23b84afd03d65",
+    measurementId: "G-QTBCSE9YD2"
+};
+
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyB0hUaqBAAaByPfhLPlIjqMwUDfcvXxBAA",
+//   authDomain: "mygitappnew-d2734.firebaseapp.com",
+//   databaseURL: "https://mygitappnew-d2734-default-rtdb.firebaseio.com",
+//   projectId: "mygitappnew-d2734",
+//   storageBucket: "mygitappnew-d2734.appspot.com",
+//   messagingSenderId: "727574800993",
+//   appId: "1:727574800993:web:91694e90917143ed01d820",
+//   measurementId: "G-PXJ25PW30W"
+// };
+
+
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyBz0XCoMwVikrX5YK01XoOo3HP_DYH4nAk",
+//   authDomain: "mygitapp-70be0.firebaseapp.com",
+//   databaseURL: "https://mygitapp-70be0-default-rtdb.firebaseio.com",
+//   projectId: "mygitapp-70be0",
+//   storageBucket: "mygitapp-70be0.appspot.com",
+//   messagingSenderId: "346636933444",
+//   appId: "1:346636933444:web:7e9f0b4f060252836e9f11",
+//   measurementId: "G-TH5Q3CM2MG"
+// };
+
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
