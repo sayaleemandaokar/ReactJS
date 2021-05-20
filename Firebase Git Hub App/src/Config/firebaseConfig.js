2b47659e1e3b235bab1b8a5b275bc9a199e2d158
sayaleemandaokar <58383890+sayaleemandaokar@users.noreply.github.com>
2021-05-20 21:05:17 +0530

    Create firebaseConfig.js

---
+++ Firebase Git Hub App/src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBz0XCoMwVikrX5YK01XoOo3HP_DYH4nAk",
+    authDomain: "mygitapp-70be0.firebaseapp.com",
+    projectId: "mygitapp-70be0",
+    storageBucket: "mygitapp-70be0.appspot.com",
+    messagingSenderId: "346636933444",
+    appId: "1:346636933444:web:7e9f0b4f060252836e9f11",
+    measurementId: "G-TH5Q3CM2MG"
+  };
